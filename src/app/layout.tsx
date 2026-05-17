ab4a39a4700cc0265a42bb163a2d86cb8dbff176
Smith Livingston <smithlivingston2005@gmail.com>
2026-05-17 13:54:02 +0530

    Icon for PWA is Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,11 +52,10 @@ export const metadata: Metadata = {
 
   icons: {
     icon:             [
-      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/miraclefm.jpg", sizes: "512x512", type: "image/jpeg" },
     ],
     apple:            [
-      { url: "/icons/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/miraclefm.jpg", sizes: "512x512", type: "image/jpeg" },
     ],
     shortcut: "/miraclefm.jpg",
   },
